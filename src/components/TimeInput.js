3196aba33e4ec2a15bcf274ea1bd820bbc52f45b
Gunnar Carlson <gunnar.carlson@gmail.com>
2019-05-12 20:17:08 -0500

    basic layout creation

---
+++ src/components/TimeInput.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import TextField from '@material-ui/core/TextField';
+
+function TimeInput() {
+    return (
+        <div>
+            <TextField type="time"></TextField>
+        </div>
+    );
+}
+
+export default TimeInput
