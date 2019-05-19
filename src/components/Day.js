8833f272641be490570bd73330d028fc5a63ccd4
Gunnar Carlson <gunnar.carlson@gmail.com>
2019-05-19 12:08:44 -0500

    More gh-pages fixes

--- src/components/Day.js
+++ src/components/Day.js
@@ -8,11 +8,18 @@ function Day(props) {
     return(
         <div className="dayContainer">
             <h3>{props.name}:</h3>
-            <h3>{props.hours}H {props.minutes}M</h3>
+            
             <div className="punchContainer">
                 {props.punches.map((punch, i) =>
                     <Punch key={i} day={props.name} time={punch} dayIndex={props.dayIndex} punchIndex={i} updatePunch={props.updatePunch}/>
                 )}
+            </div>
+            <div className="totals">
+                <span>R: {props.regularHours}H {props.regularMinutes}M</span>
+                <br />
+                <span>OT: {props.overtimeHours}H {props.overtimeMinutes}M</span>
+                <br />
+                <span>Total: {props.hours}H {props.minutes}M</span>
             </div>
         </div>
     )
