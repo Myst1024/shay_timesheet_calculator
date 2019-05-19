0a7d51ce4615be6415bc16d705e5c7e7405c1460
Gunnar Carlson <gunnar.carlson@gmail.com>
2019-05-19 11:50:05 -0500

    gh pages setup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Day from './components/Day.js';
-import Totals from './components/Totals.js'
 import './App.css';
 
 
@@ -14,43 +13,71 @@ class App extends React.Component {
           name: 'Sunday',
           punches: ['00:00','00:00','00:00','00:00','00:00','00:00','00:00','00:00'],
           hours: 0,
-          minutes: 0
+          minutes: 0,
+          regularHours: 0,
+          regularMinutes: 0,
+          overtimeHours: 0,
+          overtimeMinutes: 0,
         },
         {
           name: 'Monday',
           punches: ['00:00','00:00','00:00','00:00','00:00','00:00','00:00','00:00'],
           hours: 0,
-          minutes: 0
+          minutes: 0,
+          regularHours: 0,
+          regularMinutes: 0,
+          overtimeHours: 0,
+          overtimeMinutes: 0,
         },
         {
           name: 'Tuesday',
           punches: ['00:00','00:00','00:00','00:00','00:00','00:00','00:00','00:00'],
           hours: 0,
-          minutes: 0
+          minutes: 0,
+          regularHours: 0,
+          regularMinutes: 0,
+          overtimeHours: 0,
+          overtimeMinutes: 0,
         },
         {
           name: 'Wednesday',
           punches: ['00:00','00:00','00:00','00:00','00:00','00:00','00:00','00:00'],
           hours: 0,
-          minutes: 0
+          minutes: 0,
+          regularHours: 0,
+          regularMinutes: 0,
+          overtimeHours: 0,
+          overtimeMinutes: 0,
         },
         {
           name: 'Thursday',
           punches: ['00:00','00:00','00:00','00:00','00:00','00:00','00:00','00:00'],
           hours: 0,
-          minutes: 0
+          minutes: 0,
+          regularHours: 0,
+          regularMinutes: 0,
+          overtimeHours: 0,
+          overtimeMinutes: 0,
         },
         {
           name: 'Friday',
           punches: ['00:00','00:00','00:00','00:00','00:00','00:00','00:00','00:00'],
           hours: 0,
-          minutes: 0
+          minutes: 0,
+          regularHours: 0,
+          regularMinutes: 0,
+          overtimeHours: 0,
+          overtimeMinutes: 0,
         },
         {
           name: 'Saturday',
           punches: ['00:00','00:00','00:00','00:00','00:00','00:00','00:00','00:00'],
           hours: 0,
-          minutes: 0
+          minutes: 0,
+          regularHours: 0,
+          regularMinutes: 0,
+          overtimeHours: 0,
+          overtimeMinutes: 0,
         },
         
       ]
@@ -61,6 +88,31 @@ class App extends React.Component {
     let days = this.state.days
     days[dayIndex].hours = hours
     days[dayIndex].minutes = minutes
+
+    let totalMinutes = 0
+    const overtimeLimit = 40 * 60
+    days.forEach(function(day, i) {
+      let normalMinutes = 0
+      let overtimeMinutes = 0
+      let dayMinutes = ((day.hours * 60) + day.minutes)
+      totalMinutes += dayMinutes
+      if (totalMinutes > overtimeLimit) {
+        if (totalMinutes - dayMinutes > overtimeLimit){
+          overtimeMinutes = dayMinutes;
+        } else {
+          overtimeMinutes = (totalMinutes - overtimeLimit);
+          normalMinutes = dayMinutes - overtimeMinutes;
+        }
+      } else {
+        normalMinutes = dayMinutes
+      }
+      days[i].regularHours = Math.floor(normalMinutes/60);
+      days[i].regularMinutes = (normalMinutes % 60);
+      days[i].overtimeHours = Math.floor(overtimeMinutes/60);
+      days[i].overtimeMinutes = (overtimeMinutes % 60);
+
+    })
+
     this.setState({days: days})
   }
 
@@ -83,7 +135,6 @@ class App extends React.Component {
     var totalHours = Math.floor(totalTime / 60);
     var totalMinutes = totalTime % 60;
     this.setTotals(dayIndex, totalHours, totalMinutes)
-    console.log(totalHours, totalMinutes);
     //return <span>Hours: {totalHours} Minutes: {totalMinutes}</span>
 
 }
@@ -114,16 +165,21 @@ class App extends React.Component {
   
   render() {
     const days = this.state.days.map((day, index) => 
-      <Day 
-        key={day.name} 
-        name={day.name} 
-        dayIndex={index} 
-        punches={day.punches} 
-        updatePunch={this.updatePunch.bind(this)}
-        setTotals={this.setTotals.bind(this)}
-        hours={day.hours}
-        minutes={day.minutes}
-      />
+      <div className="day" key={day.name} >
+        <Day 
+          name={day.name} 
+          dayIndex={index} 
+          punches={day.punches} 
+          updatePunch={this.updatePunch.bind(this)}
+          setTotals={this.setTotals.bind(this)}
+          hours={day.hours}
+          minutes={day.minutes}
+          regularHours={day.regularHours}
+          regularMinutes={day.regularMinutes}
+          overtimeHours={day.overtimeHours}
+          overtimeMinutes={day.overtimeMinutes}
+        />
+      </div>
     )
 
     return (
@@ -140,7 +196,6 @@ class App extends React.Component {
           {days}
           
         </div>
-        <Totals days={this.state.days}/>
       </div>
     )
   };
